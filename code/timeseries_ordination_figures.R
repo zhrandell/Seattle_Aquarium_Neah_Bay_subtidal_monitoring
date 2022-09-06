d8f03babafa96598e12ce6936e6c02d173ea512a
randellz <randellz@oregonstate.edu>
2022-09-05 20:49:31 -0700

    edits to code
    including changepoint analysis

--- code/timeseries_ordination_figures.R
+++ code/timeseries_ordination_figures.R
@@ -1,6 +1,6 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Seattle Aquarium long-term rockfish monitoring around Neah Bay, Washington  
-## multivariate analyses community structure
+## data visualization and changepoint analysis 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -10,7 +10,11 @@
 ## startup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 rm(list = ls())
 
+
 library(tidyverse)
+library(changepoint)
+library(zoo)
+
 
 ## set your paths in a project folder 
 input <- "D:/OneDrive/Active_Projects/Neah_Bay/data_input"
@@ -18,8 +22,9 @@ output <- "D:/OneDrive/Active_Projects/Neah_Bay/data_output"
 code <- "D:/OneDrive/Active_Projects/Neah_Bay/code" 
 fig <- "D:/OneDrive/Active_Projects/Neah_Bay/figures"
 
+
 setwd(output)
-dat <- read.csv("NMDS_coords_noYOY.csv")
+dat <- read.csv("NMDS_coords.csv")
 #log_dat <- read.csv("log_NMDS_coords.csv")
 
 dat <- dat[,-1]
@@ -42,9 +47,8 @@ dat$id <-seq(1:len)
 ## calculate total abundance 
 dat <- dat %>%
   rowwise(id) %>%
-  mutate(total=sum(c(BlackDeacon, Cabezon, Canary, China, Copper, Greenling, Lingcod, 
-                     Quillback, Tiger, Widow, Yellowtail, YOY, 
-                     Vermillion, Halibut, Yelloweye)))
+  mutate(total=sum(c(Black, Cabezon, Canary, China, Copper, Greenling, Lingcod, 
+                     Quillback, Tiger, Widow, Yellowtail, Vermillion, Yelloweye)))
 
 
 ## filter by site to calculate site ellipses 
@@ -60,11 +64,109 @@ S5 <- filter.site("5")
 ## long-form data structure for time series plots 
 long <- dat %>%
   gather("spp_name", "count",
-         BlackDeacon, Cabezon, Canary, China, Copper, Greenling, Lingcod, 
-         Quillback, Tiger, Widow, Yellowtail, YOY, 
-         Vermillion, Halibut, Yelloweye) %>%
-  arrange(desc(spp_name, count))
+         Black, Cabezon, Canary, China, Copper, Greenling, Lingcod, 
+         Quillback, Tiger, Widow, Yellowtail, Vermillion, Yelloweye) %>%
+  dplyr::arrange(desc(spp_name))
 ## END data wrangling ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+## data prep for changepoint analysis ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## sum data across sites
+dat <- dat[, -c(23, 24)]
+
+
+## avg data across sites
+dat.avg <- dat %>%
+  group_by(Year) %>%
+  summarise(across(
+    .cols = -c(1:7),
+    .fns = list(avg=mean),
+    .names = "{col}"))
+
+
+
+## function to format data for changepoint 
+change.pt <- function(col, spp){
+  t1 <- dat.avg[, c(1, col)]
+  t2 <- t1 %>% spread(Year, spp)
+  t3 <- as.numeric(t2)
+  out <- cpt.meanvar(t3, Q=5)
+  return(out)
+}
+
+
+
+Black <- change.pt(2, "Black")
+Cabezon <- change.pt(3, "Cabezon")
+Canary <- change.pt(4, "Canary")
+China <- change.pt(5, "China")
+Copper <- change.pt(6, "Copper")
+Greenling <- change.pt(7, "Greenling")
+Lingcod <- change.pt(8, "Lingcod")
+Quillback <- change.pt(9, "Quillback")
+Tiger <- change.pt(10, "Tiger")
+Widow <- change.pt(11, "Widow")
+Yellowtail <- change.pt(12, "Yellowtail")
+YOY <- change.pt(13, "YOY")
+Vermillion <- change.pt(14, "Vermillion")
+Yelloweye <- change.pt(15, "Yelloweye")
+
+
+## plot changepoints 
+plot.changepts <- function(spp, full_spp, title){
+  offset <- 0.001
+  zero <- 2005
+  x1 <- spp@cpts[[1]]
+  x2 <- spp@cpts[[2]]
+  y1 <- spp@param.est[["mean"]][1]
+  y2 <- spp@param.est[["mean"]][2]
+  
+  p1 <- ggplot(dat.avg, aes(Year, full_spp)) + geom_path() + geom_point() + my.theme + x.lab + x.breaks +
+    geom_rect(data=dat.avg, aes(xmin=zero, xmax=zero+x1, ymin=y1-offset, ymax=y1+offset), color="red") +
+    geom_rect(data=dat.avg, aes(xmin=zero+x1, xmax=zero+x2, ymin=y2-offset, ymax=y2+offset), color="red") +
+    theme(axis.title.x=element_blank()) + ylab("Site averaged abundance") + ggtitle(title)
+  return(p1)
+}
+
+
+p.black <- plot.changepts(Black, dat.avg$Black, "Black & Deacon Rockfish")
+print(p.black)
+
+p.china <- plot.changepts(China, dat.avg$China, "China Rockfish")
+print(p.china)
+
+p.greenling <- plot.changepts(Greenling, dat.avg$Greenling, "Rock greenling")
+print(p.greenling)
+
+p.canary <- plot.changepts(Canary, dat.avg$Canary, "Canary Rockfish")
+print(p.canary)
+
+p.copper <- plot.changepts(Copper, dat.avg$Copper, "Copper Rockfish")
+print(p.copper)
+
+p.quill <- plot.changepts(Quillback, dat.avg$Quillback, "Quillback Rockfish")
+print(p.quill)
+
+p.YOY <- plot.changepts(YOY, dat.avg$YOY, "Young of the Year rockfish")
+print(p.YOY)
+
+p.ling <- plot.changepts(Lingcod, dat.avg$Lingcod, "Lingcod")
+print(p.ling)
+
+p.vermillion <- plot.changepts(Vermillion, dat.avg$Vermillion, "Vermillion Rockfish")
+print(p.vermillion)
+
+p.yellowEye <- plot.changepts(Yelloweye, dat.avg$Yelloweye, "Yelloweye Rockfish")
+print(p.yellowEye)
+
+p.yellowtail <- plot.changepts(Yellowtail, dat.avg$Yellowtail, "Yellowtail Rockfish")
+print(p.yellowtail)
+
+p.widow <- plot.changepts(Widow, dat.avg$Widow, "Widow Rockfish")
+print(p.widow)
 
 
 
@@ -85,8 +187,6 @@ legend.theme = theme(legend.title = element_text(size=13),
 no.legend = theme(legend.position = "none")
 
 
-
-
 ## specify hex codes for custom colors
 site.cols <- c(
   "#FFA824",  # site 1
@@ -97,12 +197,11 @@ site.cols <- c(
 )
 
 
-
 ## custom graphical params
 my.cols <- scale_color_manual(values=site.cols)
 txt.angle <- 45
 x.lab <- theme(axis.text.x = element_text(angle = txt.angle, hjust=1)) 
-x.breaks <- scale_x_continuous(breaks=seq(2005,2019,by=1))
+x.breaks <- scale_x_continuous(breaks=seq(2005,2021,by=1))
 no.leg <- theme(legend.position = "none")
 #no.x.title <- theme(axis.title.x = element_blank())
 #strip.text <- theme(strip.text.x = element_text(size=14))
@@ -168,7 +267,7 @@ windows(10.5, 8, record=T)
 
 
 setwd(output)
-spp_scores <- read.csv("log_spp_scores.csv", header=T)
+spp_scores <- read.csv("spp_scores_noYOY.csv", header=T)
 names(spp_scores)[1]<-"spp_name"
 
 
@@ -237,6 +336,25 @@ print(p5)
 
 
 
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## END of script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## change-point detection experimentation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set.seed(1)
+x=c(rnorm(100,0,1),rnorm(100,0,10))
+ansvar=cpt.var(x)
+plot(ansvar)
+print(ansvar) # identifies 1 changepoint at 100
+
+
+# change in mean
+y=c(rnorm(100,0,1),rnorm(100,5,1))
+ansmean=cpt.mean(y)
+plot(ansmean,cpt.col='blue')
+print(ansmean)
+
+# change in mean and variance
+z=c(rnorm(100,0,1),rnorm(100,2,10))
+ansmeanvar=cpt.meanvar(z)
+plot(ansmeanvar,cpt.width=3)
+print(ansmeanvar)
+## END change-point detection experimentation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
